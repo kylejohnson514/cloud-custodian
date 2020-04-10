d04a897f97d46d8afc59de1b949dee8548482ecd
Kyle Johnson <kylejohnson514@gmail.com>
2020-04-10 17:18:42 -0400

    Working test to filter referenced SGs from ingress rules. Need to add test for egress rules

--- tests/test_vpc.py
+++ tests/test_vpc.py
@@ -2063,6 +2063,42 @@ class SecurityGroupTest(BaseTest):
         manager = p.load_resource_manager()
         self.assertEqual(len(manager.filter_resources(resources)), 1)
 
+    def test_security_group_reference_ingress_filter(self):
+        factory = self.replay_flight_data("test_security_group_reference_ingress_filter")
+        p = self.load_policy(
+            {
+                "name": "security_group_reference_ingress_filter",
+                "resource": "security-group",
+                "filters": [
+                    {
+                        "type": "ingress",
+                        "SGReferences": {
+                            "key": "tag:SampleTagKey",
+                            "value": "SampleTagValue",
+                            "op": "equal"
+                        }
+                    }
+                ],
+            },
+            session_factory=factory,
+        )
+        resources = p.run()
+
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(
+            resources[0],
+            {
+                 'Description': 'Test SG with a rule referencing another SG, which is tagged with SampleTagKey=SampleTagValue',
+                 'GroupName': 'ingress-sg-reference', 'IpPermissions': [
+                    {'IpProtocol': '-1', 'IpRanges': [], 'Ipv6Ranges': [], 'PrefixListIds': [],
+                     'UserIdGroupPairs': [{'GroupId': 'sg-0f9585dc91c9c9655', 'UserId': '123456789123'}]}],
+                 'OwnerId': '123456789123', 'GroupId': 'sg-00719faf5970af89c', 'IpPermissionsEgress': [
+                    {'IpProtocol': '-1', 'IpRanges': [{'CidrIp': '0.0.0.0/0'}], 'Ipv6Ranges': [], 'PrefixListIds': [],
+                     'UserIdGroupPairs': []}], 'VpcId': 'vpc-94d1d1ee', 'MatchedIpPermissions': [
+                    {'IpProtocol': '-1', 'IpRanges': [], 'Ipv6Ranges': [], 'PrefixListIds': [],
+                     'UserIdGroupPairs': [{'GroupId': 'sg-0f9585dc91c9c9655', 'UserId': '123456789123'}]}]}
+        )
+
     def test_egress_ipv6(self):
         p = self.load_policy({
             "name": "ipv6-test",
